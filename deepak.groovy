a34a5f38e7b57c442b91c787813ce005ea5d567c
Deepak12310 <46573366+Deepak12310@users.noreply.github.com>
2019-01-12 09:43:29 +0530

    Update deepak.groovy

--- deepak.groovy
+++ deepak.groovy
@@ -1,53 +1,151 @@
-#!/usr/bin/env groovy
- 
-/**
-        * Sample Jenkinsfile for Jenkins2 Pipeline
-        * from https://github.com/hotwilson/jenkins2/edit/master/Jenkinsfile
-        * by deepakdogra98@gmail.com 
- */
- 
-import hudson.model.*
-import hudson.EnvVars
-import groovy.json.JsonSlurperClassic
-import groovy.json.JsonBuilder
-import groovy.json.JsonOutput
-import java.net.URL
- 
-try {
-node {
-stage '\u2776 Stage 1'
-echo "\u2600 BUILD_URL=${env.BUILD_URL}"
- 
-def workspace = pwd()
-echo "\u2600 workspace=${workspace}"
- 
-stage '\u2777 Stage 2'
-} // node
-} // try end
-catch (exc) {
+// Licensed under MIT
+// author : Damien Nozay
+// ---------------------------------------------------------
+// This script goes through all the jobs and checks the disk usage.
+// prints some disk usage stats as well as the retention policy.
+// ---------------------------------------------------------
 
- err = caughtError
- currentBuild.result = "FAILURE"
- String recipient = 'infra@lists.jenkins-ci.org'
- mail subject: "${env.JOB_NAME} (${env.BUILD_NUMBER}) failed",
-         body: "It appears that ${env.BUILD_URL} is failing, somebody should do something about that",
-           to: recipient,
-      replyTo: recipient,
- from: 'noreply@ci.jenkins.io'
+// e.g.:
+// 
+// JOB: playground/test-python
+//   -> lastbuild: #1 = FAILURE, time: 2015-02-12T20:56:16Z
+//   -> builds=12, average=8 KB, max=9 KB, total=97 KB, worstCase=113 KB
+// 
+// 
 
-} finally {
-  
- (currentBuild.result != "ABORTED") && node("master") {
-     // Send e-mail notifications for failed or unstable builds.
-     // currentBuild.result must be non-null for this step to work.
-     step([$class: 'Mailer',
-        notifyEveryUnstableBuild: true,
-        recipients: "deepakdogra98@gmail.com",
-        sendToIndividuals: true])
- }
- 
- // Must re-throw exception to propagate error:
- if (err) {
-     throw err
- }
+def printDiscarder(job) {
+  d = job.buildDiscarder
+  if (d) {
+     println("  -> keep: builds=(${d.daysToKeep} days, ${d.numToKeep} total); artifacts=(${d.artifactDaysToKeep} days, ${d.artifactNumToKeep} total)")
+  } else {
+     println("  -> no retention policy.")
+  }
 }
+
+import hudson.plugins.disk_usage.BuildDiskUsageAction
+import hudson.plugins.disk_usage.DiskUsageUtil
+
+def getDiskUsage(build) {
+  usage = null
+  build.getTransientActions().each {
+    action ->
+    if (action instanceof BuildDiskUsageAction) {
+      // println action.buildUsageString
+      // println action.allDiskUsage
+      usage = action
+    }
+  }
+  return usage
+}
+
+def printDiskUsage(job) {
+  maxUsage = 0
+  totalUsage = 0
+  numBuilds = 0
+  job.builds.each() {
+    build ->
+    usage = getDiskUsage(build)
+    if (usage.allDiskUsage > maxUsage) { maxUsage = usage.allDiskUsage }
+    totalUsage += usage.allDiskUsage
+    numBuilds += 1
+    // println(" * build ${build.number} - ${usage.buildUsageString}")
+  }
+  averageUsage = 0
+  if (numBuilds) { averageUsage = (totalUsage / numBuilds).longValue() }
+  worstCase = numBuilds * maxUsage
+  println("  -> builds=${numBuilds}, average=${DiskUsageUtil.getSizeString(averageUsage)}, max=${DiskUsageUtil.getSizeString(maxUsage)}, total=${DiskUsageUtil.getSizeString(totalUsage)}, worstCase=${DiskUsageUtil.getSizeString(worstCase)}")
+}
+ 
+jobs = Jenkins.instance.getAllItems()
+jobs.each { j ->
+  if (j instanceof com.cloudbees.hudson.plugins.folder.Folder) { return }
+ 
+  numbuilds = j.builds.size()
+  println 'JOB: ' + j.fullName
+  if (numbuilds == 0) {
+    println '  -> no build'
+  } else {
+    lastbuild = j.builds[numbuilds - 1]
+    println '  -> lastbuild: ' + lastbuild.displayName + ' = ' + lastbuild.result + ', time: ' + lastbuild.timestampString2
+  }
+  printDiscarder(j)
+  printDiskUsage(j)
+  println ''
+}
+ 
+''// Licensed under MIT
+// author : Damien Nozay
+// ---------------------------------------------------------
+// This script goes through all the jobs and checks the disk usage.
+// prints some disk usage stats as well as the retention policy.
+// ---------------------------------------------------------
+
+// e.g.:
+// 
+// JOB: playground/test-python
+//   -> lastbuild: #1 = FAILURE, time: 2015-02-12T20:56:16Z
+//   -> builds=12, average=8 KB, max=9 KB, total=97 KB, worstCase=113 KB
+// 
+// 
+
+def printDiscarder(job) {
+  d = job.buildDiscarder
+  if (d) {
+     println("  -> keep: builds=(${d.daysToKeep} days, ${d.numToKeep} total); artifacts=(${d.artifactDaysToKeep} days, ${d.artifactNumToKeep} total)")
+  } else {
+     println("  -> no retention policy.")
+  }
+}
+
+import hudson.plugins.disk_usage.BuildDiskUsageAction
+import hudson.plugins.disk_usage.DiskUsageUtil
+
+def getDiskUsage(build) {
+  usage = null
+  build.getTransientActions().each {
+    action ->
+    if (action instanceof BuildDiskUsageAction) {
+      // println action.buildUsageString
+      // println action.allDiskUsage
+      usage = action
+    }
+  }
+  return usage
+}
+
+def printDiskUsage(job) {
+  maxUsage = 0
+  totalUsage = 0
+  numBuilds = 0
+  job.builds.each() {
+    build ->
+    usage = getDiskUsage(build)
+    if (usage.allDiskUsage > maxUsage) { maxUsage = usage.allDiskUsage }
+    totalUsage += usage.allDiskUsage
+    numBuilds += 1
+    // println(" * build ${build.number} - ${usage.buildUsageString}")
+  }
+  averageUsage = 0
+  if (numBuilds) { averageUsage = (totalUsage / numBuilds).longValue() }
+  worstCase = numBuilds * maxUsage
+  println("  -> builds=${numBuilds}, average=${DiskUsageUtil.getSizeString(averageUsage)}, max=${DiskUsageUtil.getSizeString(maxUsage)}, total=${DiskUsageUtil.getSizeString(totalUsage)}, worstCase=${DiskUsageUtil.getSizeString(worstCase)}")
+}
+ 
+jobs = Jenkins.instance.getAllItems()
+jobs.each { j ->
+  if (j instanceof com.cloudbees.hudson.plugins.folder.Folder) { return }
+ 
+  numbuilds = j.builds.size()
+  println 'JOB: ' + j.fullName
+  if (numbuilds == 0) {
+    println '  -> no build'
+  } else {
+    lastbuild = j.builds[numbuilds - 1]
+    println '  -> lastbuild: ' + lastbuild.displayName + ' = ' + lastbuild.result + ', time: ' + lastbuild.timestampString2
+  }
+  printDiscarder(j)
+  printDiskUsage(j)
+  println ''
+}
+ 
+''
